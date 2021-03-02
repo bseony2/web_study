11b1171634c9fe4083656a26d516d27cfa110684
bseony2 <bseony2@gmail.com>
2021-03-02 23:20:32 +0900

    for Landing page

--- movie_app/client/src/components/views/LandingPage/LandingPage.js
+++ movie_app/client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import { response } from 'express';
+import React, { useEffect } from 'react'
 import { FaCode } from "react-icons/fa";
-
+import {API_KEY, API_URL} from 'client\src\components\Config.js';
 function LandingPage() {
+
+    useEffect(() =>{
+        const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => console.log(response))
+    }, [])
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{width: '100%', margin: '0'}}>
+
+            {/*Main Imagety*/ }
+
+            <div style={{width: '85%', margin: '1rem auto'}}>
+
+                <h2>Movie sby latest</h2>
+                <hr />
+                
+                {/*Movies by latest*/}
+
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <button> Load More</button>
+        </div>
+
+        </div>
     )
 }
 
