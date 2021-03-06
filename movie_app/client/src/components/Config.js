74a44029a0cd46d3d0040e4c7500e58e4428138f
bseony2 <bseony2@gmail.com>
2021-03-06 14:09:51 +0900

    너무 필요없어서...

--- movie_app/client/src/components/Config.js
+++ movie_app/client/src/components/Config.js
@@ -3,4 +3,6 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
 
-export const IMAGE_BASE = 'http://image.tmdb.org/t/p/';
+export const API_KEY = 'ddfb2f737e314580d6c0b795e0483ec3';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
